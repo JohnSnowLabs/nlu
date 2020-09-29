d88f89138c74cfe0d50e1d691c56eb27e806ed4c
Lev <agsfer@gmail.com>
2020-09-29 15:19:20 +0300

    Update style

---
+++ docs/_includes/scripts/custom.js
@@ -0,0 +1,11 @@
+/* Responsive menu
+	 ========================================================*/
+jQuery(document).ready(function($) {
+  jQuery('#responsive_menu').click(function(e) {
+      e.preventDefault();
+      jQuery(this).toggleClass('close');
+      jQuery('.top_navigation').toggleClass('open');      
+  });
+});
+
+
