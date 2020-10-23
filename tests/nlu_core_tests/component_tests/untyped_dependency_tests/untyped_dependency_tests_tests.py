51b341166d858e2aa4559176abe864aeda9ac1ce
C-K-Loan <christian.kasim.loan@gmail.com>
2020-10-23 09:54:37 +0200

    dep parser test updates

--- tests/nlu_core_tests/component_tests/untyped_dependency_tests/untyped_dependency_tests_tests.py
+++ tests/nlu_core_tests/component_tests/untyped_dependency_tests/untyped_dependency_tests_tests.py
@@ -7,11 +7,28 @@ from nlu import *
 class TestDepUntyped(unittest.TestCase):
 
     def test_dependency_untyped_model(self):
-        df = nlu.load('dep.untyped',verbose=True).predict('I love peanutbutter and jelly')
+        df = nlu.load('dep.untyped',verbose=True).predict('I love peanutbutter and jelly', output_level='document')
+        # df = nlu.load('en.classify.sarcasm',verbose=True).predict(sarcasm_df['text'])
+        print("DOCUMENT")
+        print(df.columns)
+        print(df['document'], df[['dependency','pos']])
+
+
+
+        df = nlu.load('dep.untyped',verbose=True).predict('I love peanutbutter and jelly', output_level='sentence')
         # df = nlu.load('en.classify.sarcasm',verbose=True).predict(sarcasm_df['text'])
 
+        print("SENTENCE")
         print(df.columns)
-        print(df['sentence'], df[['category','category_confidence']])
+        print(df['sentence'], df[['dependency','pos']])
+
+        df = nlu.load('dep.untyped',verbose=True).predict('I love peanutbutter and jelly', output_level='token')
+        # df = nlu.load('en.classify.sarcasm',verbose=True).predict(sarcasm_df['text'])
+        print("TOKEN")
+        print(df.columns)
+        print(df['token'], df[['dependency','pos']])
+
+
 
 
 
