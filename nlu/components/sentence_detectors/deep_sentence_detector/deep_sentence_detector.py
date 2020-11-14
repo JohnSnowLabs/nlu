5386ea01aa0a38bb0663de7cfa2c4ba141898b28
C-K-Loan <christian.kasim.loan@gmail.com>
2020-11-14 03:38:24 +0100

    Sentence Detector bugfixes

--- nlu/components/sentence_detectors/deep_sentence_detector/deep_sentence_detector.py
+++ nlu/components/sentence_detectors/deep_sentence_detector/deep_sentence_detector.py
@@ -6,9 +6,6 @@ from sparknlp.annotator import *
 class SentenDetectorDeep:
     @staticmethod
     def get_default_model():
-        return DeepSentenceDetector() \
-            .setInputCols(["document", "token", "ner_chunk"]) \
-            .setOutputCol("sentence") \
-            .setIncludePragmaticSegmenter(True) \
-            .setEndPunctuation([".", "?"])
-# --conf spark.app.name =Spark NLP
+        return SentenceDetectorDLModel()\
+            .setInputCols(["document"]) \
+            .setOutputCol("sentence")
