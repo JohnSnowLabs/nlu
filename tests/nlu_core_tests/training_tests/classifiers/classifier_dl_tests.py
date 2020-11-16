4625a4562aa58637ead8239ea2528e834aae78c5
C-K-Loan <christian.kasim.loan@gmail.com>
2020-11-16 19:37:38 +0100

    trainable clasifier tests

--- tests/nlu_core_tests/training_tests/classifiers/classifier_dl_tests.py
+++ tests/nlu_core_tests/training_tests/classifiers/classifier_dl_tests.py
@@ -13,23 +13,51 @@ class ClassifierDlTests(unittest.TestCase):
 
         test_path = '/home/loan/Documents/freelancework/jsl/nlu/4realnlugit/tests/datasets/news_category_test.csv'
 
-        train_path = '/home/loan/Documents/freelancework/jsl/nlu/4realnlugit/tests/datasets/news_category_train.csv'
+        # train_path = '/home/loan/Documents/freelancework/jsl/nlu/4realnlugit/tests/datasets/news_category_train.csv'
         test_df = pd.read_csv(test_path)
-        train_df = pd.read_csv(train_path)
+        train_df = pd.read_csv(test_path)
         train_df.columns = ['label','text']
-        pipe = nlu.load('train.classifier pos ner',verbose=True,)
+        test_df.columns = ['label','text']
+        pipe = nlu.load('train.classifier',verbose=True,)
         pipe['classifier_dl'].setMaxEpochs(2)
         df = pipe.predict(train_df)
         print(df[['category','label']])
 
-        pipe = nlu.load('train.classifier pos ner',verbose=True,)
         df = pipe.predict(test_df)
+        print(df.columns)
         print(df[['category','label']])
 
         from sklearn.metrics import classification_report
 
         print (classification_report(df['label'], df['category']))
 
+
+    def test_classifier_dl_training_stacked(self):
+        # Just put in one of the many special 'trainable' references, to load
+        # trainable components into the pipe
+        # nlu.load('ner classifier_dl bert') will only give trainable classifier dl
+        #
+
+        test_path = '/home/loan/Documents/freelancework/jsl/nlu/4realnlugit/tests/datasets/news_category_test.csv'
+
+        # train_path = '/home/loan/Documents/freelancework/jsl/nlu/4realnlugit/tests/datasets/news_category_train.csv'
+        test_df = pd.read_csv(test_path)
+        train_df = pd.read_csv(test_path)
+        train_df.columns = ['label','text']
+        test_df.columns = ['label','text']
+        pipe = nlu.load('train.classifier emotion',verbose=True,)
+        pipe['classifier_dl'].setMaxEpochs(2)
+        df = pipe.predict(train_df,output_level='document')
+        print(df.columns)
+        print(df[['category']])
+# Label column missing!
+        df = pipe.predict(test_df,output_level='document')
+        print(df.columns)
+        print(df[['category']])
+
+        from sklearn.metrics import classification_report
+
+        print (classification_report(df['label'], df['category']))
 
     def load_classifier_dl_dataset(self):
         # train_url = 'https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_train.csv'
