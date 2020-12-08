1c44148273b347340f75e2d2c07df55d41cdf212
C-K-Loan <christian.kasim.loan@gmail.com>
2020-12-08 18:29:30 +0100

    CI updates

--- tests/nlu_core_tests/training_tests/trained_pipe_tests/model_load_tests.py
+++ tests/nlu_core_tests/training_tests/trained_pipe_tests/model_load_tests.py
@@ -18,7 +18,8 @@ class PipelineLoadingTests(unittest.TestCase):
         #relative from tests/nlu_core_tests/training_tests/trained_pipe_tests
         output_file_name = 'news_category_test.csv'
         output_folder = 'classifier_dl/'
-        data_url = "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_train.csv"
+        data_url = "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_test.csv"
+
         return pd.read_csv(t.download_dataset(data_url,output_file_name,output_folder))
 
 
