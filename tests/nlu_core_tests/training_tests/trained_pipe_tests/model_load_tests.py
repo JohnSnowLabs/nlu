b559251fc80783bd854975d9ee217d3e0a29ad36
C-K-Loan <christian.kasim.loan@gmail.com>
2020-12-08 18:14:45 +0100

    CI updates

--- tests/nlu_core_tests/training_tests/trained_pipe_tests/model_load_tests.py
+++ tests/nlu_core_tests/training_tests/trained_pipe_tests/model_load_tests.py
@@ -5,7 +5,6 @@ class PipelineLoadingTests(unittest.TestCase):
 
 
     def test_pipeline_load_from_hdd(self):
-        store_path = self.get_model_dir('trained_pipe_load')
         # test_path = self.load_classifier_dl_dataset()
         # train_df = pd.read_csv(test_path)
         train_df = self.load_classifier_dl_dataset()
@@ -24,14 +23,14 @@ class PipelineLoadingTests(unittest.TestCase):
         data_dir = '~/datasets/'
         data_url = "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_train.csv"
         return pd.DataFrame(t.download_dataset(data_url,output_file_name,output_folder,data_dir))
-
-    def get_model_dir(self,suffix):
-        #relative from tests/nlu_core_tests/training_tests/trained_pipe_tests
-        output_folder = f'classifier_dl_save{suffix}/'
-        model_dir = '~/models/'
-        output_path = model_dir+output_folder
-        t.create_path_if_not_exist(output_path)
-        return output_folder
+    #
+    # def get_model_dir(self,suffix):
+    #     #relative from tests/nlu_core_tests/training_tests/trained_pipe_tests
+    #     output_folder = f'classifier_dl_save{suffix}/'
+    #     model_dir = '~/models/'
+    #     output_path = model_dir+output_folder
+    #     t.create_path_if_not_exist(output_path)
+    #     return output_folder
 
 
 
