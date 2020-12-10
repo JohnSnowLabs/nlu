05d101e979551bf5e4a7868dd1c591be8fc6ddea
C-K-Loan <christian.kasim.loan@gmail.com>
2020-12-10 19:36:38 +0100

    ci updates

--- tests/nlu_core_tests/training_tests/classifiers/classifier_dl_tests.py
+++ tests/nlu_core_tests/training_tests/classifiers/classifier_dl_tests.py
@@ -10,8 +10,7 @@ import tests.test_utils as t
 class ClassifierDlTests(unittest.TestCase):
 
     def test_classifier_dl_training(self):
-        test_path = self.load_classifier_dl_dataset()
-        test_df = pd.read_csv(test_path)
+        test_df = self.load_classifier_dl_dataset()
         train_df = test_df
         train_df.columns = ['y','text']
         test_df.columns = ['y','text']
@@ -28,8 +27,7 @@ class ClassifierDlTests(unittest.TestCase):
 
 
     def test_classifier_dl_custom_embeds_doc_level(self):
-        test_path = self.load_classifier_dl_dataset()
-        test_df = pd.read_csv(test_path)
+        test_df = self.load_classifier_dl_dataset()
         train_df = test_df
         train_df.columns = ['y','text']
         test_df.columns = ['y','text']
@@ -49,8 +47,7 @@ class ClassifierDlTests(unittest.TestCase):
         print (classification_report(df['y'], df['category']))
 
     def test_classifier_dl_custom_embeds_sentence_level(self):
-        test_path = self.load_classifier_dl_dataset()
-        test_df = pd.read_csv(test_path)
+        test_df = self.load_classifier_dl_dataset()
         train_df = test_df
         train_df.columns = ['y','text']
         test_df.columns = ['y','text']
@@ -72,8 +69,7 @@ class ClassifierDlTests(unittest.TestCase):
 
 
     def test_classifier_dl_custom_embeds_auto_level(self):
-        test_path = self.load_classifier_dl_dataset()
-        test_df = pd.read_csv(test_path)
+        test_df = self.load_classifier_dl_dataset()
         train_df = test_df
         train_df.columns = ['y','text']
         test_df.columns = ['y','text']
@@ -91,24 +87,13 @@ class ClassifierDlTests(unittest.TestCase):
         from sklearn.metrics import classification_report
 
         print (classification_report(df['y'], df['category']))
+
     def load_classifier_dl_dataset(self):
         #relative from tests/nlu_core_tests/training_tests/classifiers
         output_file_name = 'news_category_test.csv'
         output_folder = 'classifier_dl/'
-        data_dir = '../../../datasets/'
         data_url = "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_test.csv"
-        return t.download_dataset(data_url,output_file_name,output_folder,data_dir)
-    #
-    # def load_classifier_dl_dataset(self):
-    #     #relative from tests/nlu_core_tests/training_tests/classifiers
-    #     output_file_name = 'news_category_test.csv'
-    #     output_folder = 'classifier_dl/'
-    #     data_dir = t.create_dataset_dir_if_not_exist_and_get_path()
-    #     t.create_path_if_not_exist(data_dir + output_file_name)
-    #     data_url = "https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/classifier-dl/news_Category/news_category_test.csv"
-    #     return t.download_dataset(data_url,output_file_name,output_folder,data_dir)
-
-
+        return pd.read_csv(t.download_dataset(data_url,output_file_name,output_folder))
 
 if __name__ == '__main__':
     unittest.main()
