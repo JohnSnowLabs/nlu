3eab13251265a7451b94ba7d33f238e6666c55f2
C-K-Loan <christian.kasim.loan@gmail.com>
2020-12-29 20:59:50 +0100

    test updates

--- tests/nlu_core_tests/component_tests/classifier_tests/emotion_tests.py
+++ tests/nlu_core_tests/component_tests/classifier_tests/emotion_tests.py
@@ -7,8 +7,6 @@ from nlu import *
 class TestEmotion(unittest.TestCase):
 
     def test_emotion_model(self):
-        #TODO bad output level inference for some model!2
-        ## todo bug, if we Input a row with 2 sentences, where first sentence is happy and second is sad,
         # NLU will predict both as happy. If you reverse order both become sad
 
         pipe = nlu.load('emotion',verbose=True)
--- tests/nlu_core_tests/component_tests/classifier_tests/toxic_tests.py
+++ tests/nlu_core_tests/component_tests/classifier_tests/toxic_tests.py
@@ -8,22 +8,28 @@ from nlu import *
 class TestToxic(unittest.TestCase):
 
     def test_toxic_model(self):
-
+        # nlu.load('en.ner.dl.bert').predict("I like Angela Merkel")
         pipe = nlu.load('toxic',verbose=True)
-        df = pipe.predict(['You stupid man', 'You stupid woman'], output_level='sentence')
+        data = ['You are so dumb you goofy dummy', 'You stupid person with an identity that shall remain unnamed, such a filthy identity that you have go to a bad place you person!']
+        df = pipe.predict(data, output_level='sentence')
         print(df)
         print(df.columns)
-        print(df['sentence'], df[['toxic','toxic_confidence']])
-        df = pipe.predict(['You stupid man', 'You stupid woman'], output_level='document')
-        self.assertIsInstance(df.iloc[0]['toxic'],str )
+
+        print(df['sentence'], df[['toxic_classes']])
+        print(df['sentence'], df[['toxic_confidences']])
+        df = pipe.predict(data, output_level='document',metadata=True)
+
 
         print(df)
         print(df.columns)
-        print(df['document'], df[['toxic','toxic_confidence']])
-        self.assertIsInstance(df.iloc[0]['toxic'], str)
+        print(df['document'], df[['toxic_obscene_confidence']])
+        print(df['toxic_severe_toxic_confidence'], df[['toxic_insult_confidence']])
+        print(df['toxic_toxic_confidence'], df[['toxic_obscene_confidence']])
+
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
