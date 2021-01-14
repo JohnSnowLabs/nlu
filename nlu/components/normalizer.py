131af44a8171f6972ddd798427bf3d5fb2af93d2
C-K-Loan <christian.kasim.loan@gmail.com>
2021-01-15 00:29:44 +0100

    document normalizer integrated

--- nlu/components/normalizer.py
+++ nlu/components/normalizer.py
@@ -1,14 +1,21 @@
 from nlu.pipe_components import SparkNLUComponent
 
 class Normalizer(SparkNLUComponent):
-    def __init__(self, annotator_class='normalizer', language='en', component_type='normalizer', get_default=True, nlp_ref='',model=None):
-        if model != None :
-            self.model = model
+    def __init__(self, annotator_class='normalizer', language='en', component_type='normalizer', get_default=True, nlp_ref='',nlu_ref='',model=None):
+        if model != None :self.model = model
+        else :
+            if 'norm_document' in nlu_ref : annotator_class = 'document_normalizer'
+            elif 'norm' in nlu_ref : annotator_class = 'normalizer'
 
-        elif annotator_class == 'normalizer':
-            from nlu import SparkNLPNormalizer
-            if get_default : self.model =  SparkNLPNormalizer.get_default_model()
-            else : self.model =  SparkNLPNormalizer.get_pretrained_model(nlp_ref, language) # there is no pretrained API for Normalizer in SparkNLP yet
+
+            if annotator_class == 'normalizer':
+                from nlu import SparkNLPNormalizer
+                if get_default : self.model =  SparkNLPNormalizer.get_default_model()
+                else : self.model =  SparkNLPNormalizer.get_pretrained_model(nlp_ref, language) # there is no pretrained API for Normalizer in SparkNLP yet
+            elif annotator_class == 'document_normalizer':
+                from nlu import SparkNLPDocumentNormalizer
+                if get_default : self.model =  SparkNLPDocumentNormalizer.get_default_model()
+                else : self.model =  SparkNLPDocumentNormalizer.get_pretrained_model(nlp_ref, language) # there is no pretrained API for Normalizer in SparkNLP yet
 
 
         SparkNLUComponent.__init__(self, annotator_class, component_type)
---
+++ nlu/components/normalizers/document_normalizer/spark_nlp_normalizer.py
@@ -0,0 +1,12 @@
+import nlu.pipe_components
+import sparknlp
+from sparknlp.annotator import *
+
+class SparkNLPDocumentNormalizer:
+    @staticmethod
+    def get_default_model():
+        return DocumentNormalizer() \
+            .setInputCols(["document"]) \
+            .setOutputCol("normalized_document")\
+            .setAction('clean')
+
