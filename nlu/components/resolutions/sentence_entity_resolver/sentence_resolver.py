15f76e5c02754222867e914d1e37789f6c81af9e
C-K-Loan <christian.kasim.loan@gmail.com>
2021-03-08 04:27:49 +0100

    Chunk Resolver and SentenceResolver info updates

--- nlu/components/resolutions/sentence_entity_resolver/sentence_resolver.py
+++ nlu/components/resolutions/sentence_entity_resolver/sentence_resolver.py
@@ -5,11 +5,11 @@ class SentenceResolver:
     def get_pretrained_model(name, language, bucket='clinical/models'):
         return SentenceEntityResolverModel.pretrained(name, language,bucket) \
             .setInputCols(["sentence_embeddings"])\
-            .setOutputCol("resolution")
+            .setOutputCol("sentence_resolution")
 
     @staticmethod
-    def get_default_trainable_model(self):
+    def get_default_trainable_model():
         return SentenceEntityResolverApproach() \
-        .setInputCols("sentence_embeddings") \
-        .setOutputCol("resolution")\
+        .setInputCols("entities","sentence_embeddings") \
+        .setOutputCol("sentence_resolution")\
         .setLabelCol('label')
