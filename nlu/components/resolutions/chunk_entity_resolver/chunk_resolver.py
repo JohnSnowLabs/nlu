b74628b6fe7a5cb24d63ad0c8672c0fac5dd88f8
CK-Loan <christian.kasim.loan@gmail.com>
2021-04-15 00:20:31 +0200

    resolution defaults updated to k=1 and cosine dist

--- nlu/components/resolutions/chunk_entity_resolver/chunk_resolver.py
+++ nlu/components/resolutions/chunk_entity_resolver/chunk_resolver.py
@@ -4,12 +4,17 @@ class ChunkResolver:
     @staticmethod
     def get_pretrained_model(name, language, bucket='clinical/models'):
         return ChunkEntityResolverModel.pretrained(name, language,bucket) \
-            .setInputCols(["token","chunk_embeddings"])\
+            .setInputCols(["token","chunk_embeddings"]) \
+            .setDistanceFunction("COSINE") \
+            .set("COSINE") \
+            .setNeighbours(1) \
             .setOutputCol("chunk_resolution")
 
     @staticmethod
     def get_default_trainable_model():
         return ChunkEntityResolverModel() \
         .setInputCols("token","chunk_embeddings") \
-        .setOutputCol("chunk_resolution")\
-        .setLabelCol('label')
+        .setOutputCol("chunk_resolution") \
+        .setDistanceFunction("COSINE") \
+            .setNeighbours(1) \
+            .setLabelCol('label')
--- nlu/components/resolutions/sentence_entity_resolver/sentence_resolver.py
+++ nlu/components/resolutions/sentence_entity_resolver/sentence_resolver.py
@@ -4,12 +4,16 @@ class SentenceResolver:
     @staticmethod
     def get_pretrained_model(name, language, bucket='clinical/models'):
         return SentenceEntityResolverModel.pretrained(name, language,bucket) \
-            .setInputCols(['entities',"sentence_embeddings"])\
+            .setInputCols(['entities',"sentence_embeddings"]) \
+            .setDistanceFunction("COSINE") \
+            .setNeighbours(1) \
             .setOutputCol("sentence_resolution")
 
     @staticmethod
     def get_default_trainable_model():
         return SentenceEntityResolverApproach() \
         .setInputCols("entities","sentence_embeddings") \
-        .setOutputCol("sentence_resolution")\
+        .setOutputCol("sentence_resolution") \
+        .setDistanceFunction("COSINE") \
+        .setNeighbours(1) \
         .setLabelCol('label')
