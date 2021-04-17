7f189dab5cccdbe2224e08be34c5662dd23e0ee8
CK-Loan <christian.kasim.loan@gmail.com>
2021-04-17 03:59:51 +0200

    fixed bad bert_sentence componen info

--- nlu/components/embeddings/sentence_bert/BertSentenceEmbedding.py
+++ nlu/components/embeddings/sentence_bert/BertSentenceEmbedding.py
@@ -6,13 +6,13 @@ class BertSentence:
     def get_default_model():
         return   BertSentenceEmbeddings.pretrained() \
         .setInputCols("sentence") \
-        .setOutputCol("bert_sentence")
+        .setOutputCol("sentence_embeddings")
 
     @staticmethod
     def get_pretrained_model(name, language, bucket=None):
         return   BertSentenceEmbeddings.pretrained(name,language,bucket) \
             .setInputCols('sentence') \
-            .setOutputCol("bert_sentence")
+            .setOutputCol("sentence_embeddings")
 
 
 
