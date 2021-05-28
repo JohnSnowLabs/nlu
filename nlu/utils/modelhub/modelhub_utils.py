c2339ebd9cd1bdf30d3663749bab03f6914d4451
C-K-Loan <christian.kasim.loan@gmail.com>
2021-05-28 22:35:28 +0200

    improved NLU to NLP ref

--- nlu/utils/modelhub/modelhub_utils.py
+++ nlu/utils/modelhub/modelhub_utils.py
@@ -5,16 +5,15 @@ import requests
 class ModelHubUtils():
     modelhub_json_url = 'https://nlp.johnsnowlabs.com/models.json'
     data = requests.get(modelhub_json_url).json()
-    """Pipe Level logic oprations and utils"""
+    """Modelhub utils"""
     @staticmethod
     def NLU_ref_to_NLP_ref(nlu_ref: str,lang: str = None) -> str:
         """Resolve a Spark NLU reference to q NLP reference.
-
         Args :
         NLU_ref : which nlu model's nlp refrence to return.
         lang : what language is the model in.
         """
-        nlu_namespaces_to_check = [nlu.NameSpace.pretrained_pipe_references,nlu.NameSpace.pretrained_models_references,nlu.NameSpace.pretrained_healthcare_model_references,nlu.NameSpace.licensed_storage_ref_2_nlu_ref ,nlu.NameSpace.storage_ref_2_nlu_ref ]#component_alias_references  ]
+        nlu_namespaces_to_check = [nlu.NameSpace.pretrained_pipe_references,nlu.NameSpace.pretrained_models_references,nlu.NameSpace.pretrained_healthcare_model_references,nlu.NameSpace.licensed_storage_ref_2_nlu_ref ,nlu.NameSpace.storage_ref_2_nlu_ref ]#]
         for dict_ in nlu_namespaces_to_check:
             if lang:
                 if lang in dict_.keys():
@@ -27,7 +26,10 @@ class ModelHubUtils():
                         for reference in dict_[lang]:
                             if reference ==nlu_ref:
                                 return  dict_[lang][reference]
+        for _nlp_ref, nlp_ref_type in nlu.NameSpace.component_alias_references.items():
+            if _nlp_ref == nlu_ref : return  nlp_ref_type[0]
 
+        return ''
 
 
     @staticmethod
@@ -40,16 +42,18 @@ class ModelHubUtils():
         """
 
         # getting spark refrence for given nlu refrence
+        if nlu_refrence =='' : return  'https://nlp.johnsnowlabs.com/models'
         if nlu_refrence.split(".")[0] not in  nlu.AllComponentsInfo().all_languages:
             nlu_refrence = "en."+nlu_refrence
         nlp_refrence= ModelHubUtils.NLU_ref_to_NLP_ref(nlu_refrence)
         if nlp_refrence == None :
             print(f"{nlp_refrence}         {nlu_refrence}")
+            return 'https://nlp.johnsnowlabs.com/models'
         else :
             for model in ModelHubUtils.data :
                 if (model['language'] in nlu_refrence.split(".") or model['language'] in nlp_refrence.split('_')) and model['name'] == nlp_refrence:
-
                     return f"https://nlp.johnsnowlabs.com/{model['url']}"
+        return 'https://nlp.johnsnowlabs.com/models'
 
     @staticmethod
     def return_json_entry(nlu_refrence:str) -> dict:
