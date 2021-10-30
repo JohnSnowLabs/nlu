37032e5c6b2d4abcd7257b24ac9ca30605882ee5
C-K-Loan <christian.kasim.loan@gmail.com>
2021-10-30 08:25:48 +0200

    setup update

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 REQUIRED_PKGS = [
     # 'pyspark>=2.4.0,<2.5',
-    'spark-nlp>=3.2.0,<3.3.0',
+    'spark-nlp>=3.3.0,<3.4.0',
     'numpy',
     'pyarrow>=0.16.0',
     'pandas',
