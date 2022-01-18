4d91a3d838eebcada021c79f2e2f639d012bfca3
C-K-Loan <christian.kasim.loan@gmail.com>
2022-01-18 09:39:49 +0100

    Universe Module introduced and integrated and OCR integrated

--- nlu/utils/environment/authentication.py
+++ nlu/utils/environment/authentication.py
@@ -1,63 +1,130 @@
 from nlu.utils.environment.env_utils import *
 
 
-def install_and_import_healthcare(JSL_SECRET):
-    """ Install Spark-NLP-Healthcare PyPI Package in current enviroment if it cannot be imported and liscense provided"""
+def import_or_install_licensed_lib(JSL_SECRET, lib='healthcare'):
+    """ Install Spark-NLP-Healthcare PyPI Package in current environment if it cannot be imported and license
+    provided """
     import importlib
+    hc_module_name = 'sparknlp_jsl'
+    hc_pip_package_name = 'spark-nlp-jsl'
+    hc_display_name = ' Spark NLP for Healthcare'
+    ocr_pip_package_name = 'spark-ocr'
+    ocr_module_name = 'sparkocr'
+    ocr_display_name = ' Spark OCR'
+
+    lib_version = JSL_SECRET.split('-')[0]
+    if lib == 'healthcare':
+        target_import = hc_module_name
+        target_install = hc_pip_package_name
+        display_name = hc_display_name
+    elif lib == 'ocr':
+        target_import = ocr_module_name
+        target_install = ocr_pip_package_name
+        display_name = ocr_display_name
+        # OCR version is suffixed with spark version
+        if is_env_pyspark_2_3():
+            lib_version = lib_version + '+spark23'
+        if is_env_pyspark_2_4():
+            lib_version = lib_version + '+spark24'
+        if is_env_pyspark_3_0() or is_env_pyspark_3_1():
+            lib_version = lib_version + '+spark30'
+
+    else:
+        raise ValueError(f'Invalid install licensed install target ={lib}')
+
     try:
-        importlib.import_module('sparknlp_jsl')
+        # Try importing, if it fails install the pacakge
+        importlib.import_module(target_import)
     except ImportError:
+        # Install package since its missing
         import pip
-        print("Spark NLP Healthcare could not be imported. Installing latest spark-nlp-jsl PyPI package via pip...")
-        hc_version = JSL_SECRET.split('-')[0]
+        print(f"{display_name} could not be imported. Installing latest {target_install} PyPI package via pip...")
         import pyspark
         pip_major_version = int(pip.__version__.split('.')[0])
         if pip_major_version in [10, 18, 19, 20]:
-            # for these versions pip module does not support installing, we install via OS command.
+            # for these versions pip module does not support installing from Python, we install via OS command.
             os.system(
-                f'pip install spark-nlp-jsl=={hc_version} --extra-index-url https://pypi.johnsnowlabs.com/{JSL_SECRET}')
+                f'{sys.executable} -m pip install {target_install}=={lib_version} --extra-index-url https://pypi.johnsnowlabs.com/{JSL_SECRET}')
         else:
-            pip.main(['install', f'spark-nlp-jsl=={hc_version}', '--extra-index-url',
+            pip.main(['install', f'{target_install}=={lib_version}', '--extra-index-url',
                       f'https://pypi.johnsnowlabs.com/{JSL_SECRET}'])
     finally:
+        # Import module after installing package
         import site
         from importlib import reload
         reload(site)
-        globals()['sparknlp_jsl'] = importlib.import_module('sparknlp_jsl')
+        globals()[target_import] = importlib.import_module(target_import)
 
 
-def authenticate_enviroment(SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
+def authenticate_enviroment_HC(SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
     """Set Secret environ variables for Spark Context"""
     os.environ['SPARK_NLP_LICENSE'] = SPARK_NLP_LICENSE
     os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
     os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
 
+def authenticate_enviroment_OCR(SPARK_OCR_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY):
+    """Set Secret environ variables for Spark Context"""
+    os.environ['SPARK_OCR_LICENSE'] = SPARK_OCR_LICENSE
+    os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
+    os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
+
+def authenticate_enviroment_HC_and_OCR(SPARK_NLP_LICENSE,SPARK_OCR_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY ):
+    """Set Secret environ variables for Spark Context"""
+    authenticate_enviroment_HC(SPARK_NLP_LICENSE,AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    authenticate_enviroment_OCR(SPARK_OCR_LICENSE,AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+
+
+def get_authenticated_spark_HC(HC_LICENSE, HC_SECRET, AWS_ACCESS_KEY, AWS_SECRET_KEY, gpu):
+    pass
+
+
+def get_authenticated_spark_OCR(OCR_LICENSE, OCR_SECRET, AWS_ACCESS_KEY, AWS_SECRET_KEY, gpu):
+    pass
+
+
+def get_authenticated_spark_HC_and_OCR(HC_LICENSE, HC_SECRET, OCR_LICENSE, OCR_SECRET, AWS_ACCESS_KEY, AWS_SECRET_KEY,
+                                       gpu):
+    import_or_install_licensed_lib(HC_SECRET, 'healthcare')
+    import_or_install_licensed_lib(OCR_SECRET, 'ocr')
+    authenticate_enviroment_HC_and_OCR(HC_LICENSE, OCR_LICENSE, AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    import sparknlp_jsl
+    import sparkocr
+    import sparknlp
+
+    params = {"spark.driver.memory": "16G", "spark.kryoserializer.buffer.max": "2000M",
+              "spark.driver.maxResultSize": "2000M"}
+
+    HC_version = HC_SECRET.split('-')[0]
+    OS_version = sparknlp.version()
+    spark = sparkocr.start(secret=OCR_SECRET, nlp_secret=HC_SECRET, nlp_version=OS_version, nlp_internal=HC_version)
+    spark.sparkContext.setLogLevel('ERROR')
+
 
 def get_authenticated_spark(SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, JSL_SECRET, gpu=False, ):
     """
-    Authenticates enviroment if not already done so and returns Spark Context with Healthcare Jar loaded
+    Authenticates environment if not already done so and returns Spark Context with Healthcare Jar loaded
     0. If no Spark-NLP-Healthcare, install it via PyPi
     1. If not auth, run authenticate_enviroment()
 
     """
     import sparknlp
-    authenticate_enviroment(SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-    install_and_import_healthcare(JSL_SECRET)
+    authenticate_enviroment_HC(SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    import_or_install_licensed_lib(JSL_SECRET)
     import sparknlp_jsl
-    params = {"spark.driver.memory":"16G",
-              "spark.kryoserializer.buffer.max":"2000M",
-              "spark.driver.maxResultSize":"2000M"}
+    params = {"spark.driver.memory": "16G",
+              "spark.kryoserializer.buffer.max": "2000M",
+              "spark.driver.maxResultSize": "2000M"}
 
-    if is_env_pyspark_2_3(): return sparknlp_jsl.start(JSL_SECRET, spark23=True, gpu=gpu,params=params)
-    if is_env_pyspark_2_4(): return sparknlp_jsl.start(JSL_SECRET, spark24=True, gpu=gpu,params=params)
+    if is_env_pyspark_2_3(): return sparknlp_jsl.start(JSL_SECRET, spark23=True, gpu=gpu, params=params)
+    if is_env_pyspark_2_4(): return sparknlp_jsl.start(JSL_SECRET, spark24=True, gpu=gpu, params=params)
     if is_env_pyspark_3_0() or is_env_pyspark_3_1():
-        return sparknlp_jsl.start(JSL_SECRET, gpu=gpu, public=sparknlp.version(),params=params)
+        return sparknlp_jsl.start(JSL_SECRET, gpu=gpu, public=sparknlp.version(), params=params)
     print(f"Current Spark version {get_pyspark_version()} not supported!")
     raise ValueError
 
 
 def is_authorized_enviroment():
-    """Check if auth secrets are set in enviroment"""
+    """Check if auth secrets are set in environment"""
     SPARK_NLP_LICENSE = os.getenv('SPARK_NLP_LICENSE')
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
--- nlu/utils/environment/offline_load_utils.py
+++ nlu/utils/environment/offline_load_utils.py
@@ -1,66 +1,65 @@
-import os,sys, json
-
-import nlu
+import os, json
 from nlu.pipe.pipe_component import SparkNLUComponent
-# from nlu.pipe.pipe_logic import PipelineQueryVerifier
-from nlu.pipe.pipeline import  *
-from nlu.pipe.pipe_component import SparkNLUComponent
-from pyspark.ml import PipelineModel
-
 from sparknlp.annotator import *
 
 def is_pipe(model_path):
-    """Check wether there is a pyspark pipe stored in path"""
+    """Check wether there is a pyspark component_list stored in path"""
     return 'stages' in os.listdir(model_path)
+
+
 def is_model(model_path):
     """Check wether there is a model stored in path"""
-    return'metadata' in os.listdir(model_path)
+    return 'metadata' in os.listdir(model_path)
+
 
 def get_model_class(model_path):
     """Extract class from a model saved in model_path"""
-    with open(model_path+'/stages/part-00000') as json_file:
+    with open(model_path + '/stages/part-00000') as json_file:
         java_class = json.load(json_file)['class']
         pyth_class = java_class.split('.')[-1]
-    return java_class,pyth_class
+    return java_class, pyth_class
 
-def verify_and_create_model(model_path:str, nlu_referenced_requirements = [], nlp_referenced_requirements = [], test_data='Hello world from John Snow Labs '):
+
+def verify_and_create_model(model_path: str, nlu_referenced_requirements=[], nlp_referenced_requirements=[],
+                            test_data='Hello world from John Snow Labs '):
     """
      Build model with requirements
      Figures out class name by checking metadata json file
      assumes metadatra is always called part-00000
     """
-    with open(model_path+'/metadata/'+'part-00000') as json_f:
+    with open(model_path + '/metadata/' + 'part-00000') as json_f:
         class_name = json.load(json_f)['class'].split('.')[-1]
         # The last element in the Class name can be used to just load the model from disk!
         # Just call eval on it, which will give you the actual Python class reference which should have a .load() method
-        try :
+        try:
             m = eval(class_name).load(model_path)
         except:
             from nlu.utils.environment.offline_load_utils_licensed import verify_model_licensed
             m = verify_model_licensed(class_name, model_path)
 
-
-
-    component_type,nlu_anno_class, = resolve_annotator_class_to_nlu_component_info(class_name)
+    component_type, nlu_anno_class, = resolve_annotator_class_to_nlu_component_info(class_name)
     # Wrap model with NLU Custom Model class so the NLU pipeline Logic knows what to do with it
     c = CustomModel(annotator_class=nlu_anno_class, component_type=component_type, model=m)
     if nlu.hard_offline_checks:
-        storage_ref =  m.getStorageRef() if hasattr(m,'getStorageRef')  else -1
+        storage_ref = m.getStorageRef() if hasattr(m, 'getStorageRef') else -1
         feeds_from_embeddings = False
-        for inp in c.info.inputs() :
-            if 'embed' in inp : feeds_from_embeddings = True
-        if type(m) not in [SentenceEmbeddings,ChunkEmbeddings]  and storage_ref != -1 and feeds_from_embeddings:
+        for inp in c.info.inputs():
+            if 'embed' in inp: feeds_from_embeddings = True
+        if type(m) not in [SentenceEmbeddings, ChunkEmbeddings] and storage_ref != -1 and feeds_from_embeddings:
             # Check storage ref is matching either some NLU or NLP ref.
-            if not check_if_storage_ref_exists(storage_ref) :
+            if not check_if_storage_ref_exists(storage_ref):
                 pass
-            raise ValueError("When loading a model from disk with embeddings in it's inputs or you must set the Storage Reference attribute on the model."
-                             "Additionaly, the configured Storage Reference must either match to a NLU-Model-Reference or a Spark-NLP-Model-Reference."
-                             "To view all references visit the modelshub or view https://github.com/JohnSnowLabs/nlu/blob/master/nlu/spellbook.py "
-                             "You can set a storage reference via calling model.setStorageRef(storage_reference)"
-                             "To disable this check, run nlu.disable_hard_offline_checks()")
+            raise ValueError(
+                "When loading a model from disk with embeddings in it's inputs or you must set the Storage Reference attribute on the model."
+                "Additionaly, the configured Storage Reference must either match to a NLU-Model-Reference or a Spark-NLP-Model-Reference."
+                "To view all references visit the modelshub or view https://github.com/JohnSnowLabs/nlu/blob/master/nlu/spellbook.py "
+                "You can set a storage reference via calling model.setStorageRef(storage_reference)"
+                "To disable this check, run nlu.disable_hard_offline_checks()")
 
     return c
-def resolve_annotator_class_to_nlu_component_info(anno_class ='LemmatizerModel'):
+
+
+def resolve_annotator_class_to_nlu_component_info(anno_class='LemmatizerModel'):
     """
     SparkNLUComponent.__init__(self, annotator_class, component_type)
     RECURISIVELY SEARCH through NLU COMPONENTS source code for each class for a given <CLASSS>
@@ -68,26 +67,27 @@ def resolve_annotator_class_to_nlu_component_info(anno_class ='LemmatizerModel')
     In the folder containing that found file there will be the component_json info we need!]\
     """
     import nlu
-    p = nlu.nlu_package_location+'components/'
+    p = nlu.nlu_package_location + 'components/'
     # Check if class has pretrained. If not, dont add .
 
     import os
     for dirpath, dirs, files in os.walk(p):
         # search for folder that has the component info for that anno_class
         for filename in files:
-            if '.py' not in filename or '.pyc' in filename : continue
-            fname = os.path.join(dirpath,filename)
+            if '.py' not in filename or '.pyc' in filename: continue
+            fname = os.path.join(dirpath, filename)
             # check annotator name and also getter is in file. This should only be true for the correct annotator creator files
-            if (test_check_if_string_in_file(fname, anno_class) and test_check_if_string_in_file(fname, 'def get_.*:', True)) and\
-                    (test_check_if_string_in_file(fname, 'return *'+ anno_class, True)):
-
+            if (test_check_if_string_in_file(fname, anno_class) and test_check_if_string_in_file(fname, 'def get_.*:',
+                                                                                                 True)) and \
+                    (test_check_if_string_in_file(fname, 'return *' + anno_class, True)):
                 parts = dirpath.split('/')
                 component_type = parts[-2]
                 nlu_anno_class = parts[-1]
                 component_type = component_type[:-1]
-                return component_type,nlu_anno_class,
+                return component_type, nlu_anno_class,
     print("COULD NOT FIND COMPONENT INFO FOR ANNO_CLASS", anno_class)
     return False
+
 
 def test_check_if_string_in_file(file_name, string_to_search, regex=False):
     """ Check if any line in the file contains given string """
@@ -99,14 +99,14 @@ def test_check_if_string_in_file(file_name, string_to_search, regex=False):
         # Read all lines in the file one by one
         for line in read_obj:
             # For each line, check if line contains the string
-            if regex :
-                    if len(re.findall(string_to_search,line)) > 0 : return True
-            else :
+            if regex:
+                if len(re.findall(string_to_search, line)) > 0: return True
+            else:
                 if string_to_search in line: return True
     return False
 
 
-def NLP_ref_to_NLU_ref(nlp_ref,lang) :
+def NLP_ref_to_NLU_ref(nlp_ref, lang):
     """Resolve a Spark NLP reference to a NLU reference"""
     nlu_namespaces_to_check = [nlu.Spellbook.pretrained_pipe_references, nlu.Spellbook.pretrained_models_references]
     for dict_ in nlu_namespaces_to_check:
@@ -118,52 +118,56 @@ def NLP_ref_to_NLU_ref(nlp_ref,lang) :
 
 class CustomModel(SparkNLUComponent):
     """ Builds a NLU Components with component info"""
-    def __init__(self, annotator_class='sentiment_dl',  component_type='classifier', model = None):
+
+    def __init__(self, annotator_class='sentiment_dl', component_type='classifier', model=None):
         self.model = model
         SparkNLUComponent.__init__(self, annotator_class, component_type)
         # Make sure input/output cols match up with NLU defaults
-        if len(self.info.spark_input_column_names) == 1 :
+        if len(self.info.spark_input_column_names) == 1:
             model.setInputCols(self.info.spark_input_column_names[0])
-        else :
+        else:
             model.setInputCols(self.info.spark_input_column_names)
 
-        if len(self.info.spark_output_column_names) == 1 :
+        if len(self.info.spark_output_column_names) == 1:
             model.setOutputCol(self.info.spark_output_column_names[0])
-        else :
+        else:
             model.setOutputCol(self.info.spark_output_column_names)
 
-
-
-    # pipe = NLUPipeline()
-    # pipe.add(c)
+    # component_list = NLUPipeline()
+    # component_list.add(os_components)
     #
     # # get requirements
     # if len(nlu_referenced_requirements)==0 and len(nlp_referenced_requirements) == 0 :
-    #     pipe = PipelineQueryVerifier.check_and_fix_nlu_pipeline(pipe)
-    #     res = pipe.predict(test_data)
+    #     component_list = PipelineQueryVerifier.check_and_fix_nlu_pipeline(component_list)
+    #     res = component_list.predict(test_data)
     #     print(res)
     #     return res
     # elif len(nlu_referenced_requirements) >0:
     #     for r in nlu_referenced_requirements:
-    #         pipe.add(nlu.pipe.component_resolution.nlu_ref_to_component(r))
+    #         component_list.add(nlu.component_list.component_resolution.nlu_ref_to_component(r))
     # elif len(nlp_referenced_requirements) > 0:
     #     for r in nlu_referenced_requirements:
     #         # map back to NLU ref
-    #         pipe.add(nlu.pipe.component_resolution.nlu_ref_to_component(NLP_ref_to_NLU_ref(r)))
-    # # run pipe with dependencies
-    # pipe = PipelineQueryVerifier.check_and_fix_nlu_pipeline(pipe)
-    # res = pipe.predict(test_data)
+    #         component_list.add(nlu.component_list.component_resolution.nlu_ref_to_component(NLP_ref_to_NLU_ref(r)))
+    # # run component_list with dependencies
+    # component_list = PipelineQueryVerifier.check_and_fix_nlu_pipeline(component_list)
+    # res = component_list.predict(test_data)
     # print(res)
     # return res
+
+
 def check_if_storage_ref_exists(storage_ref):
     """Check if storage ref exists as a NLP ref or NLU ref in the spellbook"""
-    spaces = [nlu.spellbook.Spellbook.pretrained_models_references, nlu.Spellbook.pretrained_healthcare_model_references]
-    for space in spaces :
-        if check_namespace_for_storage_ref(space,storage_ref) : return True
+    spaces = [nlu.spellbook.Spellbook.pretrained_models_references,
+              nlu.Spellbook.pretrained_healthcare_model_references]
+    for space in spaces:
+        if check_namespace_for_storage_ref(space, storage_ref): return True
     return False
+
+
 def check_namespace_for_storage_ref(space, storage_ref):
     """Check if storage ref exists as a NLP ref or NLU ref in a given namespace"""
     for lang, mappings in space.items():
         for nlu_ref, nlp_ref in mappings.items():
-            if nlu_ref == storage_ref or nlp_ref == storage_ref : return True
+            if nlu_ref == storage_ref or nlp_ref == storage_ref: return True
     return False
