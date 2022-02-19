2030a3fa7bac2250f247fc3de1d69c7cff108319
C-K-Loan <christian.kasim.loan@gmail.com>
2022-02-19 03:18:22 +0100

    updated spell checker tests

--- tests/nlu_core_tests/component_tests/pre_processing_tests/spell_tests.py
+++ tests/nlu_core_tests/component_tests/pre_processing_tests/spell_tests.py
@@ -1,25 +1,24 @@
 import unittest
-from tests.test_utils import get_sample_pdf_with_labels, get_sample_pdf, get_sample_sdf, get_sample_pdf_with_extra_cols, get_sample_pdf_with_no_text_col ,get_sample_spark_dataframe
-from nlu import *
+import nlu
 
-class TestSpellCheckers (unittest.TestCase):
+
+class TestSpellCheckers(unittest.TestCase):
 
     def test_spell_context(self):
-        pipe = nlu.load('spell', verbose=True )
-        df = pipe.predict('I liek penut butter and jellli', output_level='sentence',drop_irrelevant_cols=False, metadata=True, )
+        pipe = nlu.load('en.spell', verbose=True)
+        df = pipe.predict('I liek penut buttr and jelly', drop_irrelevant_cols=False, metadata=True, )
         for c in df.columns: print(df[c])
-    #
-    # def test_spell_sym(self):
-    #     component_list = nlu.load('spell.symmetric', verbose=True )
-    #     df = component_list.predict('I liek penut butter and jellli', output_level='sentence',drop_irrelevant_cols=False, metadata=True, )
-    #     for os_components in df.columns: print(df[os_components])
-    #
-    # def test_spell_norvig(self):
-    #     component_list = nlu.load('spell.norvig', verbose=True )
-    #     df = component_list.predict('I liek penut butter and jellli', output_level='sentence',drop_irrelevant_cols=False, metadata=True, )
-    #     for os_components in df.columns: print(df[os_components])
+
+    def test_spell_sym(self):
+        component_list = nlu.load('spell.symmetric', verbose=True)
+        df = component_list.predict('I liek penut buttr and jelly', drop_irrelevant_cols=False, metadata=True, )
+        for os_components in df.columns: print(df[os_components])
+
+    def test_spell_norvig(self):
+        component_list = nlu.load('spell.norvig', verbose=True)
+        df = component_list.predict('I liek penut buttr and jelly', drop_irrelevant_cols=False, metadata=True, )
+        for os_components in df.columns: print(df[os_components])
 
 
 if __name__ == '__main__':
     unittest.main()
-
