40725f63d1ae9237c94f740ac6c7ed171b98bc49
C-K-Loan <christian.kasim.loan@gmail.com>
2022-04-29 04:04:16 +0200

    bugfixed resolver extraction for HPO

--- nlu/pipe/extractors/extractor_methods/helper_extractor_methods.py
+++ nlu/pipe/extractors/extractor_methods/helper_extractor_methods.py
@@ -71,74 +71,108 @@ def extract_maximum_confidence(row, configs):
             }
 
 
+def unpack_HPO_codes(row, k):
+    # Case: HPO
+    UMLS_codes = []
+    ORPHA_CODES = []
+    MSH_CODES = []
+    SNOMED_CODES = []
+    OMIM_CODES = []
+
+    for resolution in row[k]:
+        k_candidates = resolution.split(':::')
+        for candidate in k_candidates:
+            # There is 0 to 5 alt terminologies, 1 per HPO_codes. If one is missing, we have to append None
+            alt_terminologies = candidate.split('||')
+            UMLS_ok, ORPHA_ok, MSH_ok, SNOMED_ok, OMIM_ok = False, False, False, False, False
+            for alt in alt_terminologies:
+                if 'UMLS' in alt:
+                    UMLS_codes.append(alt)
+                    UMLS_ok = True
+                elif 'ORPHA' in alt:
+                    ORPHA_CODES.append(alt)
+                    ORPHA_ok = True
+                elif 'MSH' in alt:
+                    MSH_CODES.append(alt)
+                    MSH_ok = True
+                elif 'SNOMED' in alt:
+                    SNOMED_CODES.append(alt)
+                    SNOMED_ok = True
+                elif 'OMIM' in alt:
+                    OMIM_CODES.append(alt)
+                    OMIM_ok = True
+            # Detect which of the 0 to 5 alt terminologies are missing and add None for them
+            if not UMLS_ok:
+                UMLS_codes.append(None)
+            if not ORPHA_ok:
+                ORPHA_CODES.append(None)
+            if not MSH_ok:
+                MSH_CODES.append(None)
+            if not SNOMED_ok:
+                SNOMED_CODES.append(None)
+            if not OMIM_ok:
+                OMIM_CODES.append(None)
+    return UMLS_codes, ORPHA_CODES, MSH_CODES, SNOMED_CODES, OMIM_CODES,
+            # Write into dict
+
+
 def extract_resolver_all_k_subfields_splitted(row, configs):
     ''' Extract all metadata fields for sentence resolver annotators and splits all _k_ fields on ::: , relevant for all_k_result, all_k_resolutions, al_k_distances, all_k_cosine_distances
     pop_meta_list should be true, if outputlevel of pipe is the same as the resolver component we are extracting here for
     ::: for icd
-    || for HCC
+    || in HCC splits billable/status/code, always aligns. in HPO splits resolutions in alterantive terminologies, not always aligns
+    ::: splits Mappings of resolutions, i.e. len(split(:::)) == number_of_entities  == number of resolutions
+    Special case HPO :MeSH/SNOMED/UMLS/ORPHA/OMIM
+
+
+unpack_resolutions = lambda x : x.split(':::')
+unpack_k_aux_label = lambda x : x.split('||')
+# each of the k resolutions as K aux label.
+# for HPO, each aux label, can have 0 to 5 extra terminolgies
+unpack_terms = lambda x: x.split('')
+
+unpacked = list(map(lambda x: list(map(unpack_k_aux_label, unpack_resolutions(x))), row[k] ))
+
+for key, g in itertools.groupby(unpacked, lambda x : x.split(':')[0]):
+    print(key,list(g))
+
+unpacked[2]
+
+    :UMLS
     '''
+
+    HPO_CODES = ['UMLS', 'ORPHA', 'MSH', 'SNOMED', 'OMIM']
     prefix = 'meta_' + configs.output_col_prefix + '_'
     res = {}
     for k in row.keys():
         if '_k_' in k:
             if '||' in row[k][0]:
-                # billable code handling
-                f = lambda s: list(map(lambda x: x.split("||"), s.split(':::')))
-                # Triple assignment so we unpkack properly
-                res[prefix + 'billable'], \
-                res[prefix + 'hcc_status'], \
-                res[prefix + 'hcc_code'] = zip(*map(lambda x: zip(*x), map(f, row[k])))
-                # Casting from tuple to list or we get problems during pd explode
-                h = lambda z: list(map(lambda r: list(r), z))
-                res[prefix + 'billable'] = h(res[prefix + 'billable'])
-                res[prefix + 'hcc_status'] = h(res[prefix + 'hcc_status'])
-                res[prefix + 'hcc_code'] = h(res[prefix + 'hcc_code'])
+
+                if any(x in row[k][0] for x in HPO_CODES):
+                    # Case : HPO
+                    res[prefix + 'k_UMLS_codes'], \
+                    res[prefix + 'k_ORPHA_codes'], \
+                    res[prefix + 'k_MESH_codes'], \
+                    res[prefix + 'k_SNOMED_codes'], \
+                    res[prefix + 'k_OMIM_codes'] = unpack_HPO_codes(row, k)
+
+                else:
+                    # CASE : billable code handling
+                    f = lambda s: list(map(lambda x: x.split("||"), s.split(':::')))
+                    # Triple assignment so we unpack properly
+                    res[prefix + 'billable'], \
+                    res[prefix + 'hcc_status'], \
+                    res[prefix + 'hcc_code'] = zip(*map(lambda x: zip(*x), map(f, row[k])))
+                    # Casting from tuple to list or we get problems during pd explode
+                    h = lambda z: list(map(lambda r: list(r), z))
+                    res[prefix + 'billable'] = h(res[prefix + 'billable'])
+                    res[prefix + 'hcc_status'] = h(res[prefix + 'hcc_status'])
+                    res[prefix + 'hcc_code'] = h(res[prefix + 'hcc_code'])
 
             elif ':::' in row[k][0]:
-                # General code handling
+                # CASE : General code handling
                 res[k.replace('results', 'codes')] = list(map(lambda x: x.split(':::'), row[k]))
         else:
             # Any other metadata field hadling
             res[k] = row[k]
     return res
-    # if we pop, this means we extract array elements into single elements, i.e. ['hello'] will be 'hello'. We can do this if field is on same level as ppipe
-    #
-    # if configs.pop_meta_list:
-    #     if is_code:
-    #         return {k.replace('results', 'codes'):
-    #                     row[k][0].split(":::") if '_k_' in k else
-    #                     row[k][0] for k in row.keys()}
-    #     elif is_billable:
-    #         # billable
-    #         res = {}
-    #         for k in row.keys():
-    #             if '_k_' in k:
-    #                 billable, hcc_status, hcc_code = row[k].split('||')
-    #                 res['billable'] = billable
-    #                 res['hcc_status'] = hcc_status
-    #                 res['hcc_code'] = hcc_code
-    #             else:
-    #                 res[k] = row[k][0]
-    #     else:
-    #         return {k: row[k] for k in row.keys()}
-    #
-    # else:
-    #     if is_code:
-    #         return {k.replace('results', 'codes'):
-    #                     row[k][0].split(":::") if '_k_' in k else
-    #                     row[k] for k in row.keys()}
-    #     elif is_billable:
-    #         # billable
-    #         res = {}
-    #         for k in row.keys():
-    #             if '_k_' in k:
-    #                 billable, hcc_status, hcc_code = row[k].split('||')
-    #                 res['billable'] = billable
-    #                 res['hcc_status'] = hcc_status
-    #                 res['hcc_code'] = hcc_code
-    #             else:
-    #                 res[k] = row[k]
-    #     else:
-    #         return {k: row[k][0] for k in row.keys()}
-# z[['meta_icd10cm_code_billable', 'meta_icd10cm_code_hcc_status','meta_icd10cm_code_hcc_code']]
-# df['meta_icd10cm_code_billable'].apply(lambda r : list(map(list, r))).values
