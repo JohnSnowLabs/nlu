55c83a429b38a53fb01476142c20862e7b05bfc2
ckl <christian.kasim.loan@gmail.com>
2022-05-20 10:09:28 +0200

    tests added and setup upgraded

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 REQUIRED_PKGS = [
-    'spark-nlp>=3.4.2,<3.5.0',
+    'spark-nlp>=3.4.4,<3.5.0',
     'numpy',
     'pyarrow>=0.16.0',
     'pandas>=1.3.5',
@@ -38,7 +38,7 @@ setup(
     #
     # There are some restrictions on what makes a valid project name
     # specification here:    # https://packaging.python.org/specifications/core-metadata/#name
-    name='nlu',  # Required #nlu
+    name='nlu_tmp',  # Required #nlu
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
@@ -46,7 +46,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='3.4.3',  # Required
+    version='3.4.4',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
