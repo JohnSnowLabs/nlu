10a6c12f9ceaf5d3db9147da73cbe9c1055c9264
C-K-Loan <christian.kasim.loan@gmail.com>
2022-07-14 04:56:45 +0200

    added more tests for question span classifier data conversion

--- tests/nlu_core_tests/component_tests/span_classifier_tests/span_question_data_conversion_tests.py
+++ tests/nlu_core_tests/component_tests/span_classifier_tests/span_question_data_conversion_tests.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 
 import pandas as pd
@@ -15,50 +16,57 @@ class SpanQuestionConversionTestCase(unittest.TestCase):
     c = 'Heisenberg was a German theoretical physicist and oen of the key pioneers of quantum mechanics.'
     spark = sparknlp.start()
 
-    def validate_conversion_result(self,spark_df):
+    def validate_conversion_result(self, spark_df):
         spark_df.show()
         self.assertTrue(NLP_FEATURES.RAW_QUESTION in spark_df.columns)
         self.assertTrue(NLP_FEATURES.RAW_QUESTION_CONTEXT in spark_df.columns)
 
-
     def test_str_conversion(self):
-        data  =  f'{self.q}|||{self.c}'
+        data = f'{self.q}|||{self.c}'
         self.validate_conversion_result(DataConversionUtils.question_str_to_sdf(data, self.spark)[0])
 
-
     def test_str_iterable_conversion(self):
-        data  =  f'{self.q}|||{self.c}'
-        data = [data,data,data,data,data]
+        data = f'{self.q}|||{self.c}'
+        data = [data, data, data, data, data]
         self.validate_conversion_result(DataConversionUtils.question_str_iterable_to_sdf(data, self.spark)[0])
+        ps = pd.Series(data)
+        self.validate_conversion_result(DataConversionUtils.question_str_iterable_to_sdf(ps, self.spark)[0])
 
     def test_tuple_conversion(self):
-        pass
+        data = (self.q, self.c)
+        self.validate_conversion_result(DataConversionUtils.question_tuple_to_sdf(data, self.spark)[0])
 
     def test_tuple_iterable_conversion(self):
-        pass
+        data = (self.q, self.c)
+        data = [data, data, data, data, data]
+        self.validate_conversion_result(DataConversionUtils.question_tuple_iterable_to_sdf(data, self.spark)[0])
+
+        ps = pd.Series(data)
+        self.validate_conversion_result(DataConversionUtils.question_tuple_iterable_to_sdf(ps, self.spark)[0])
+
 
     def test_sdf_conversion(self):
-        data  = pd.DataFrame({'question':[self.q],
-                              'context': [self.c]
-                              })
+        data = pd.DataFrame({'question': [self.q],
+                             'context': [self.c]
+                             })
         data = self.spark.createDataFrame(data)
 
         self.validate_conversion_result(DataConversionUtils.question_sdf_to_sdf(data, self.spark)[0])
-        data  = pd.DataFrame({'c1':[self.q],
-                              'c2': [self.c]
-                              })
+        data = pd.DataFrame({'c1': [self.q],
+                             'c2': [self.c]
+                             })
         data = self.spark.createDataFrame(data)
 
         self.validate_conversion_result(DataConversionUtils.question_sdf_to_sdf(data, self.spark)[0])
 
     def test_pdf_conversion(self):
-        data  = pd.DataFrame({'question':[self.q],
-                            'context': [self.c]
-                            })
+        data = pd.DataFrame({'question': [self.q],
+                             'context': [self.c]
+                             })
         self.validate_conversion_result(DataConversionUtils.question_pdf_to_sdf(data, self.spark)[0])
-        data  = pd.DataFrame({'c1':[self.q],
-                              'c2': [self.c]
-                              })
+        data = pd.DataFrame({'c1': [self.q],
+                             'c2': [self.c]
+                             })
         self.validate_conversion_result(DataConversionUtils.question_pdf_to_sdf(data, self.spark)[0])
 
 
