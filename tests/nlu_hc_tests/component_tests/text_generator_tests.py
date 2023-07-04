90b02d3bda882b91b1a359e58fbf1688d9d6f2bd
GADDE SAI SHAILESH <69344247+gadde5300@users.noreply.github.com>
2023-07-04 17:36:17 +0530

    Update text_generator_tests.py

--- tests/nlu_hc_tests/component_tests/text_generator_tests.py
+++ tests/nlu_hc_tests/component_tests/text_generator_tests.py
@@ -3,33 +3,25 @@ import sys
 import unittest
 
 import nlu
-# import secrets as sct
+import tests.secrets as sct
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
 text_generator_spells = [
     'en.text_generator.biomedical_biogpt_base'
-    # 'en.summarizer.clinical_jsl_augmented',
-    # 'en.summarizer.biomedical_pubmed',
-    # 'en.summarizer.generic_jsl',
-    # 'en.summarizer.clinical_questions',
-    # 'en.summarizer.radiology',
-    # 'en.summarizer.clinical_guidelines_large',
-    # 'en.summarizer.clinical_laymen',
 ]
 
 
 class MedicalTextGeneratorTests(unittest.TestCase):
     def test_medical_text_generator(self):
-        SPARK_NLP_LICENSE = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE3MDQwNjcyMDAsImlhdCI6MTY3Mjk2MzIwMCwidW5pcXVlX2lkIjoiZGQ0MzE4ZTYtOGRhOS0xMWVkLTgyNjAtY2ViMjJiMTM3OTk4Iiwic2NvcGUiOlsibGVnYWw6aW5mZXJlbmNlIiwibGVnYWw6dHJhaW5pbmciLCJmaW5hbmNlOmluZmVyZW5jZSIsImZpbmFuY2U6dHJhaW5pbmciLCJvY3I6aW5mZXJlbmNlIiwib2NyOnRyYWluaW5nIiwiaGVhbHRoY2FyZTppbmZlcmVuY2UiLCJoZWFsdGhjYXJlOnRyYWluaW5nIl19.Uw5z6ihpLukV9sBVZn4SRZmgshmLaIFHc_KqNGKejS7Yj4b3m0pM7FMRBx2BJ5rzIPQJD0P0Qv-vK42Ze71BS4_TDe0r52UltmxX0K1R4ijUbK3gA0qYJMSRZnFSKIocZ7TRxXcACJeHsqnMkp6um0D7abrdKMSdzEM87TAOX0sO8H29rhW8UKz5eiE3o45hMMcYuxFv5zbJr9X7pxZkbVmI72Mbq8Pq0PXzKIct1S85IhKo22tlhgGeo_CLGZkDsM9735QiBTqZ8olX5sFpqTy4cDMuoX5odR8VBumf37w80NYEIZlt_vOWaXEgWvYGDhjYxJ-YbUv0bT9kQ4TmHA"
-        AWS_ACCESS_KEY_ID = "AKIASRWSDKBGLU62MMFE"
-        AWS_SECRET_ACCESS_KEY = "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"
-        JSL_SECRET = "4.4.3-d6a8f771226d032db7dccc1eae0635d5fe68d696"
+        SPARK_NLP_LICENSE = sct.SPARK_NLP_LICENSE
+        AWS_ACCESS_KEY_ID = sct.AWS_ACCESS_KEY_ID
+        AWS_SECRET_ACCESS_KEY = sct.AWS_SECRET_ACCESS_KEY
+        JSL_SECRET = sct.JSL_SECRET
         nlu.auth(
             SPARK_NLP_LICENSE, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, JSL_SECRET
         )
-        # b = BertSentenceEmbeddings.pretrained('sbiobert_base_cased_mli','en','clinical/models')
 
 
         for s in text_generator_spells:
