dea9a4549bd93091d67ed2cc8f68f147650af50a
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2023-10-08 23:59:32 +0200

    bump version

--- nlu/__init__.py
+++ nlu/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '5.0.2'
+__version__ = '5.0.3'
 
 import nlu.utils.environment.env_utils as env_utils
 
--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='5.0.0',  # Required
+    version='5.0.3',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
