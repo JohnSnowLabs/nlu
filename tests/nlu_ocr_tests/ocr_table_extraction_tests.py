5b59c7f060e9f078b79a79163cccd9c6f1f88bf8
GADDE SAI SHAILESH <69344247+gadde5300@users.noreply.github.com>
2023-12-24 03:24:15 +0530

    Update ocr_table_extraction_tests.py

--- tests/nlu_ocr_tests/ocr_table_extraction_tests.py
+++ tests/nlu_ocr_tests/ocr_table_extraction_tests.py
@@ -4,14 +4,8 @@ import sys
 sys.path.append(os.getcwd())
 import unittest
 import nlu
-
-os.environ["PYTHONPATH"] = "F:/Work/repos/nlu"
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-from johnsnowlabs import nlp, visual
-
-# nlp.install(json_license_path='license.json',visual=True)
-nlp.start(visual=True)
+nlu.auth(sct.SPARK_NLP_LICENSE,sct.AWS_ACCESS_KEY_ID,sct.AWS_SECRET_ACCESS_KEY,sct.JSL_SECRET, sct.OCR_LICENSE, sct.OCR_SECRET)
+# nlu.auth(SPARK_NLP_LICENSE,AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,JSL_SECRET, OCR_LICENSE, OCR_SECRET)
 
 class OcrTest(unittest.TestCase):
 
@@ -33,26 +27,26 @@ class OcrTest(unittest.TestCase):
         ---> for NON SELECTABLE TEXT ImageTableDetector + ImageTableCellDetector + ImageCellsToTextTable
         ---> For text whci his selectable DocToTextTable3.
         """
-        # img_path = 'tests/datasets/ocr/table_pdf_highlightable_text/data.pdf'
-        # p = nlu.load('pdf2table',verbose=True)
-        # dfs = p.predict(img_path)
-        # for df in dfs :
-        #     print(df)
+        img_path = 'tests/datasets/ocr/table_pdf_highlightable_text/data.pdf'
+        p = nlu.load('pdf2table',verbose=True)
+        dfs = p.predict(img_path)
+        for df in dfs :
+            print(df)
 
     def test_PPT_table_extraction(self):
-        f1 = '54111.ppt'
+        f1 = 'tests/datasets/ocr/table_PPT/54111.ppt'
         f2 ='tests/datasets/ocr/table_PPT/mytable.ppt'
         p = nlu.load('ppt2table',verbose=True)
         dfs = p.predict([f1    ])
         for df in dfs :
             print(df)
-    #
-    # def test_DOC_table_extraction(self):
-    #     f1 = 'tests/datasets/ocr/docx_with_table/doc2.docx'
-    #     p = nlu.load('doc2table',verbose=True)
-    #     dfs = p.predict([f1])
-    #     for df in dfs :
-    #         print(df)
+    
+    def test_DOC_table_extraction(self):
+        f1 = 'tests/datasets/ocr/docx_with_table/doc2.docx'
+        p = nlu.load('doc2table',verbose=True)
+        dfs = p.predict([f1])
+        for df in dfs :
+            print(df)
 
 
 if __name__ == '__main__':
