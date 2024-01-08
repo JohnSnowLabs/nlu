37857416c43de917f019aadc4ace8f86e9e29909
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2024-01-08 01:24:59 +0100

    update to run all tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,7 +9,7 @@ import colorama
 sys.path.append(os.getcwd())
 sys.path.append(os.getcwd() + '/tests')
 sys.path.append(os.getcwd() + '/tests/utils')
-from utils import one_per_lib
+from utils import all_tests
 _TEST_TIMEOUT = 60*60*20
 
 def run_cmd_and_check_succ(
@@ -62,8 +62,8 @@ if __name__ == '__main__':
     # So to prevent JVM-OOM we need to run each test in a separate process
 
     logs = {}
-    total_tests = len(one_per_lib)
-    for i, test_params in enumerate(one_per_lib):
+    total_tests = len(all_tests)
+    for i, test_params in enumerate(all_tests):
         print(f"{'#' * 10} Running test {i} of {total_tests}  with config {test_params} {'#' * 10}")
         logs[i] = {}
         logs[i]['test_data'] = test_params
