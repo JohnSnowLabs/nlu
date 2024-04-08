9467995e7417a131d519e98f1575fb758060e293
Christian Kasim Loan <christian.kasim.loan@gmail.com>
2024-04-08 18:45:12 +0200

    decomment class

--- nlu/components/embeddings/sentence_mpnet/MPNetSentenceEmbedding.py
+++ nlu/components/embeddings/sentence_mpnet/MPNetSentenceEmbedding.py
@@ -1,18 +1,18 @@
-# from sparknlp.annotator import MPNetEmbeddings
-#
-#
-# class MPNetSentence:
-#     @staticmethod
-#     def get_default_model():
-#         return MPNetEmbeddings.pretrained() \
-#             .setInputCols(["documents"]) \
-#             .setOutputCol("mpnet_embeddings")
-#
-#     @staticmethod
-#     def get_pretrained_model(name, language, bucket=None):
-#         return MPNetEmbeddings.pretrained(name,language,bucket) \
-#             .setInputCols(["documents"]) \
-#             .setOutputCol("mpnet_embeddings")
-#
-#
-#
+from sparknlp.annotator import MPNetEmbeddings
+
+
+class MPNetSentence:
+    @staticmethod
+    def get_default_model():
+        return MPNetEmbeddings.pretrained() \
+            .setInputCols(["documents"]) \
+            .setOutputCol("mpnet_embeddings")
+
+    @staticmethod
+    def get_pretrained_model(name, language, bucket=None):
+        return MPNetEmbeddings.pretrained(name,language,bucket) \
+            .setInputCols(["documents"]) \
+            .setOutputCol("mpnet_embeddings")
+
+
+
